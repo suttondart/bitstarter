3462c784a8ba6b01ceb5f0b21cf17e88b128227e
suttondart <ciaran.cullen@gmail.com>
2013-07-10 14:49:58 +0000

    Updated buffers in web.js

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+
+
+var mybuffer = buffer(60);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -10,3 +14,10 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+fs.readFileSync('index.html', mybuffer);
+
+buffer.toString("utf-8")
+
+console.log(mybuffer.toString('utf8', 0, len));
